ecc5fc310fe6f08ee7f8116d28d4880b646475f7
apallier <alexis.pallier@protonmail.com>
2026-08-08 21:05:26 +0200

    feat: add logo image

--- site/src/util/getOGImage.ts
+++ site/src/util/getOGImage.ts
@@ -8,7 +8,14 @@ function getBasePath(): string {
   return themeConfig.general.seo.url;
 }
 
-export function getOGImage(slug: string) {
+export function getOGImage(slug?: string) {
   let basePath: string = getBasePath();
+
+  // Pages without a slug (accueil, 404) have no generated card: use the site logo
+  // instead of letting crawlers pick the first image found in the page.
+  if (!slug) {
+    return `${basePath}${themeConfig.general.logo}`;
+  }
+
   return `${basePath}/og/${slug}.png`;
 }
